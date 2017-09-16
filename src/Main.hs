416bc3858b425108f272e183fbcd8b0f5f3a82a6
Unknown <ichumonji@gmail.com>
2017-09-16 17:31:02 +0100

    Automated parsing
    Switched to Aeson to parse data, greatly reducing the necessary work to parse stuff. Still need to change SymeUI to ajust to this change.

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,32 @@ module Main where
 
 import Control.Monad.Trans(liftIO)
 import Graphics.UI.Gtk
+import Data.Map as M
 import SymeUI
+import Methodinno
+import MethManage
 
+main= do symeui
+
+{--
 main :: IO ()
-main = do symeui
+main = do 
+    initGUI
+    symeUI <- loadSymeui "src/SymeUI.glade"
+    mapMeth <- getMapMeth "src/onche.cod"
+    initSymeSignals symeUI
+    initMainTvw symeUI mapMeth
+    mainGUI
+
+
+--getSymeData :: SymeUI 
+--            -> M.Map Int Methode Result of onche.cod parsing
+initMainTvw s m = do
+    let listofNames = Prelude.map (nom.snd) (M.toList m)
+    let listofIDs   = Prelude.map fst (M.toList m)
+    let joinedlist = Prelude.zipWith (\a b -> (show(a),b)) listofIDs listofNames
+    store <- listStoreNew joinedlist
+    cellLayoutSetAttributes (rescolumn s) (celltxt s) store $ \x -> [cellText :=  snd x]
+    cellLayoutSetAttributes (idcolumn s) (cellidtxt s) store $ \x -> [cellText :=  fst x]
+    treeViewSetModel (restreeview s) store
+--}
--- src/Symebot.hs
+++ src/Symebot.hs
@@ -2,35 +2,56 @@
 
 module Symebot(
  RawO(..)
-,defO 
-,symeScrape
+ ,test
+ ,defO 
+ ,symeScrape
 )
 where
-import Text.HTML.Scalpel
-import System.Environment
+
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
 import Data.List
-import Data.List.Split
-import Data.Time
-import Data.Maybe
+import Network.HTTP.Conduit
+import System.Environment
+
+newtype RawOList = RawOList {rawOList::[RawO]} deriving(Show)
+instance FromJSON RawOList where
+  parseJSON (Object o) = RawOList <$> o .: "results"
+
 data RawO = RawO {titlep::String
                  ,univp::String
-                 ,domp::[String]
-                 ,fkeyp::String}
+                 ,fkeyp::Int
+                 ,domp::[Object]}
                  deriving (Show)
-defO = RawO {titlep="",univp="",domp=[""],fkeyp=""}
+instance FromJSON RawO where
+  parseJSON (Object v) = 
+    RawO  <$> v.: "title"
+          <*> v.: "university_name"
+          <*> v.: "id"
+          <*> v.: "subjects"
+--Precious test function, to be used within ghci to test parsing strategies
+--before implementing them
+test = do  
+         let jSON = simpleHttp "https://www.fun-mooc.fr/fun/api/courses/?rpp=5000&page1"
+         d <- (eitherDecode <$> jSON ) :: IO (Either String RawOList)
+         case d of 
+           Left err -> putStrLn err
+           Right ps -> print (domp ((rawOList ps)!!1))        
+--Default value of parsing result, used in various places of the program
+defO :: RawO
+defO = RawO {titlep="NO_RESULT",univp="",domp=[],fkeyp=(-1)}
 
+symeScrape :: [String] -> IO([RawO])
 symeScrape [sourcetype,source] | sourcetype == "file" = scrapeFromFile source
                                | sourcetype == "url"  = scrapeFromURL source
-                               | otherwise            = print "succ"  >> return []
+                               | otherwise            = print "dank"  >> return []
 symeScrape _ = print "Usage: handleArgs [file|url] fichier|url" >> return []
 
-scrapeFromFile fp = do  c <- readFile fp
-                        let i = scrapeStringLike c apiResults
-                        parseByFlavour fp i
+scrapeFromFile fp = do  let c = B.readFile fp
+                        parseByFlavour fp c
 
-scrapeFromURL url = do  i <- scrapeURL url apiResults
-                        print i
-                        parseByFlavour url i 
+scrapeFromURL url = do  let c = simpleHttp url
+                        parseByFlavour url c 
 
 --Ajouter ici les différentes stratégies de parsing
 parseByFlavour s i | "fun-mooc" `isInfixOf` s = do funmoocFlavour i
@@ -38,47 +59,7 @@ parseByFlavour s i | "fun-mooc" `isInfixOf` s = do funmoocFlavour i
 
 --Stratégie pour le site fun-mooc. Cherche titre du cours, université, domaine
 --et clef étrangère et les renvoie sous forme de liste.
-funmoocFlavour i = do let courses = separateCourses i
-                      let lignedcourses = fmap (map lines) courses
-                      let titles = map (extractedContent "title") <$> courses
-                      let univs = map (extractedContent "university_name") <$> courses
-                      let fkeys = map (extractedContent "id") <$> courses
-                      let domains =  fmap (map listOfDomains) lignedcourses
-                      let fmOutput = rawData (fromMaybe [] titles) (fromMaybe [] univs) (fromMaybe [] domains) (fromMaybe [] fkeys)
-                      return fmOutput
-
-rawData (x:xs) (y:ys) (z:zs) (f:fs) = RawO {titlep=(fromMaybe "NOTITLE" x)
-                                           ,univp=(fromMaybe "NOUNIV" y)
-                                           ,domp=z
-                                           ,fkeyp=(fromMaybe "NOKEY" f)}:rawData xs ys zs fs
-rawData _ _ _ _                     = []
---Extrait la première occurence d'une balise JSON dans une chaîne donnée
-extractedContent ::String           --Intitulé que l'on souhaite extraire
-                -> String           --Chaîne dans laquelle chercher le terme
-                -> Maybe String     --Chaîne trouvée
-extractedContent f c = if l == [] 
-                       then Nothing
-                       else Just (((filter (\x->x/='\"')).(!!1).(splitOn (f++"\":")).(!!0)) l)
-                            where 
-                            l = (filter (\x -> f `isInfixOf` x).lines) c
---Extrait la liste des thématiques des MOOC depuis un JSon formatté sous forme
---de string
-listOfDomains::[String] -> [String]
-listOfDomains l = snd(foldl f (False,[]) l)
-                where
-                f (in_delim,domain) x | "subjects" `isInfixOf` x = (True,domain)
-                                      | "],"      `isInfixOf` x  = (False, domain)
-                                      | otherwise                = if in_delim
-                                                                   then (True, x:domain)
-                                                                   else (False, domain)
-
-apiResults :: Scraper String [String]
-apiResults = texts "pre"
-
---Dans fun-mooc, les chaînes décrivant deux cours différent sont séparées par deux tabulations
---ATTENTION: tester pour éviter l'ambiguïté tab/espace!
---Le premier élément de apiResults est une balise GET, qu'on ignore, d'où le !!1
-separateCourses = fmap ((endBy "},\n        {").(!!1))
-
-delSpace = filter (\x -> x/=' ')
-
+funmoocFlavour i = do d <- (eitherDecode <$> i) :: IO (Either String RawOList)
+                      case d of 
+                        Left err -> return [defO]
+                        Right ps -> return (rawOList ps)
